d4a543e2f72cddeb6b570c224db78d4463944534
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2021-04-21 17:01:55 +0200

    update derivative matrix and homogenious D-H matrix

--- D-H-Jacobian.cpp
+++ D-H-Jacobian.cpp
@@ -28,11 +28,16 @@ public:
   enum class Type { Prismatic, Revolute };
 
 protected:
-  double alpha;
-  double d;
-  double a;
-  double theta;
-  Type type;
+
+    //init robot pos
+    double alpha;
+    double d;
+    double a;
+    double theta;
+
+    double DOF_Value;
+
+    Type type;
 
 public:
 
@@ -46,27 +51,69 @@ public:
     double _theta)
   {
     type = _type;
+
     alpha = _alpha;
     d = _d;
     a = _a;
     theta = _theta;
+
+    DOF_Value = 0;
+
   }
 
   Matrix4x4d GetAsMatrix()
   {
-    assert(0);
-    return Matrix4x4d();
+
+      double DTheta = 0;
+      double Dd = 0;
+
+      if (type==Type::Revolute)
+      {
+          DTheta = DOF_Value;
+      }
+      if (type==Type::Prismatic)
+      {
+          Dd = DOF_Value;
+      }
+
+      Matrix4x4d solution;
+      solution <<
+      cos( theta + DTheta ),    -cos( alpha ) * sin( theta + DTheta ),     sin( alpha ) * sin( theta + DTheta ),    a * cos(theta + DTheta ),
+      sin( theta + DTheta ),     cos( alpha ) * cos( theta + DTheta ),    -sin( alpha ) * cos( theta + DTheta ),    a * sin(theta + DTheta ),
+            0,                          sin( alpha ),                               cos( alpha ),                             d + Dd,
+            0,                                  0,                                          0,                                  1;
+
+      return solution;
   }
 
-  Matrix4x4d GetDerivativeAsMatrix()
+  void SetDOF(double DeltaParameter)
   {
-    assert(0);
+      DOF_Value = DeltaParameter;
+  }
 
-    if (type == Type::Prismatic)
-      return Matrix4x4d();
+  double GetDOF()
+  {
+      return DOF_Value;
+  }
 
-    if (type == Type::Revolute)
-      return Matrix4x4d();
+  Matrix4x4d GetDerivativeAtCurrentParametrizationAsMatrix()
+  {
+      Matrix4x4d result;
+
+      if (type == Type::Prismatic)
+          result <<
+          0, 0, 0, 0,
+          0, 0, 0, 0,
+          0, 0, 0, 1,
+          0, 0, 0, 0;
+
+      if (type == Type::Revolute)
+          result <<
+          -sin(theta + DOF_Value ),     -cos( alpha ) * cos( theta + DOF_Value ),   sin( alpha ) * cos( theta + DOF_Value ),    -a * sin( theta + DOF_Value ),
+           cos(theta + DOF_Value ),     -cos( alpha ) * sin( theta + DOF_Value ),   sin( alpha ) * sin( theta + DOF_Value ),     a * cos( theta + DOF_Value ),
+                    0,                                      0,                                          0,                                          0,
+                    0,                                      0,                                          0,                                          0;
+
   }
 
 };
