a6b80de7403817ce8e38017f14c3c6e5ffa6b3fa
Horea Caramizaru <horea.caramizaru@cti.pub.ro>
2021-05-11 15:22:12 +0200

    update LoadAndWriteAnimation....

--- tests/LoadAndWriteAnimation_LUA_KukaRobot_Test.cpp
+++ tests/LoadAndWriteAnimation_LUA_KukaRobot_Test.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[]) {
 
 
     Model model;
-    if (!Addons::LuaModelReadFromFile ("kuka.lua", &model, false)) {
+    if (!Addons::LuaModelReadFromFile ("LUA/kuka.lua", &model, false)) {
         std::cerr << "Error loading lua file" << std::endl;
         abort();
     }
@@ -42,12 +42,15 @@ int main(int argc, char *argv[]) {
         << EE_worldSpace_position << std::endl << std::endl
         << EE_worldSpace_orientation << std::endl;
 
-    Vector3d A4_worldSpace_position = CalcBodyToBaseCoordinates(model,  q_start, A4_id,Vector3d(0,0,0),true);
-    Eigen::Matrix3d A4_wordSpace_orientation = CalcBodyWorldOrientation (model,  q_start, A4_id,false);
+    //Vector3d A4_worldSpace_position = CalcBodyToBaseCoordinates(model,  q_start, A4_id,Vector3d(0,0,0),true);
+    //Eigen::Matrix3d A4_wordSpace_orientation = CalcBodyWorldOrientation (model,  q_start, A4_id,false);
+    Vector3d Target;
+    Target << -0.3, -0.2, 0.3;
 
     InverseKinematicsConstraintSet cs;
-    cs.AddPointConstraint (EE_id,Vector3d(0,0,0), A4_worldSpace_position);
-    cs.AddOrientationConstraint  (EE_id, A4_wordSpace_orientation);
+    //cs.AddPointConstraint (EE_id,Vector3d(0,0,0), A4_worldSpace_position);
+    //cs.AddOrientationConstraint  (EE_id, A4_wordSpace_orientation);
+    cs.AddPointConstraint( EE_id, Vector3d().setZero(), Target);
 
     // run Inverse Kinematic
     bool ret =  InverseKinematics(model, q_start, cs, q);
